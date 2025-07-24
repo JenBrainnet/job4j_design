1d35b69b92ff7fbecfd93f96e75507a39350a49a
JenBrainnet <benebrak01@gmail.com>
2025-07-24 21:53:55 +0300

    6. Read the configuration file [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,54 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines()
+                    .filter(s -> !s.isEmpty() && !s.startsWith("#"))
+                    .forEach(s -> {
+                        String[] parts = s.split("=", 2);
+                        if (parts.length < 2 || parts[0].isEmpty() || parts[1].isEmpty()) {
+                            throw new IllegalArgumentException("Invalid format: expected \"key=value\" pattern");
+                        }
+                        values.put(parts[0], parts[1]);
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner output = new StringJoiner(System.lineSeparator());
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines().forEach(output::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("data/app.properties"));
+    }
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenPairWithEqualsInValue() {
+        String path = "./data/pair_with_equals_in_value.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("key1")).isEqualTo("value1=");
+        assertThat(config.value("key2")).isEqualTo("=value2");
+        assertThat(config.value("key3")).isEqualTo("value=3");
+    }
+
+    @Test
+    void whenPairWithCommentsAndEmptyStrings() {
+        String path = "./data/pair_with_comment_and_empty_strings.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("key1")).isEqualTo("value1");
+        assertThat(config.value("")).isNull();
+        assertThat(config.value("# Comment")).isNull();
+    }
+
+    @Test
+    void whenPairWithInvalidFormat() {
+        String path = "./data/pair_with_invalid_format.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Invalid format: expected \"key=value\" pattern");
+    }
+
+}
