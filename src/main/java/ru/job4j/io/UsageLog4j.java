041af56e2e891f6659a134cc4bd7d77af8cf0b6c
JenBrainnet <benebrak01@gmail.com>
2026-03-12 20:48:39 +0200

    3. Slf4j. Logging variables. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,11 +8,23 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        LOG.debug("User info name : {}, age : {}", name, age);
+        byte byteValue = 1;
+        short shortValue = 2;
+        int intValue = 3;
+        long longValue = 4L;
+        float floatValue = 5.5F;
+        double doubleValue = 6.6D;
+        char charValue = 'A';
+        boolean booleanValue = false;
+        LOG.info(
+                "Primitive data types in Java: byte - {}, short - {}, int - {}, "
+                        + "long - {}, float - {}, double - {}, char - {}, boolean - {}",
+                byteValue, shortValue, intValue, longValue,
+                floatValue, doubleValue, charValue, booleanValue
+        );
     }
 
 }
